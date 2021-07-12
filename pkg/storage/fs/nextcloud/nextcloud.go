13aad3914490a1764993242b316348872f8e5dd3
Michiel de Jong <michiel@unhosted.org>
2021-07-12 10:54:05 +0200

    new endpoint

--- pkg/storage/fs/nextcloud/nextcloud.go
+++ pkg/storage/fs/nextcloud/nextcloud.go
@@ -78,7 +78,7 @@ type Action struct {
 
 func (nc *nextcloud) doUpload(r io.ReadCloser) error {
 	// See https://github.com/pondersource/sciencemesh-nextcloud/issues/13
-	endPoint := "http://nc/apps/sciencemesh/@alice/test"
+	endPoint := "http://nc/apps/sciencemesh/~alice/files/test.txt"
 
 	fmt.Printf("\nUPLOADING IT TO %s!\n\n", endPoint)
 
